387733d932c820a26c6109550ec953a7428a1c83
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2021-03-23 11:10:09 +0100

    Update code

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,7 +5,18 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div``;
 
 export default ProgressBar;
